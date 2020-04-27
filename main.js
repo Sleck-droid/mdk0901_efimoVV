1372d5d4655cc9ef7b9a5dfdbd83fa7816cbd3d5
Максим <efimo07@mail.ru>
2020-04-27 13:00:14 +0300

    zadanie5

--- main.js
+++ main.js
@@ -14,3 +14,31 @@ books[4].querySelector('h2>a').textContent = 'Книга 3. this и Протот
 
 // 4
 document.querySelector('.adv').remove();
+
+// 5
+    // 2 глава
+    function sortLi(liList) {
+        let sortedLi = [];
+        for(li in liList) {
+            for(subLi of liList) {
+                if(subLi.textContent.includes(`Глава ${li}:`)) {
+                    sortedLi.push(subLi);
+                }
+            }
+        }
+        return sortedLi
+    }
+    
+    sortLi(books[0].querySelectorAll('li')).reduce(function(prev, it, i, ar){
+        ar[i-1].after(ar[i]);
+    });
+    
+    books[0].querySelectorAll('li')[books[0].querySelectorAll('li').length-2].after(books[0].querySelectorAll('li')[2]);
+    
+    sortLi(books[5].querySelectorAll('li')).reduce(function(prev, it, i, ar){
+        ar[i-1].after(ar[i]);
+    });
+    
+    books[5].querySelectorAll('li')[books[5].querySelectorAll('li').length-1].before(books[5].querySelectorAll('li')[3]);
+    books[5].querySelectorAll('li')[books[5].querySelectorAll('li').length-2].before(books[5].querySelectorAll('li')[2]);
+    
